ffebccfa0f157ebd1544d3f9a6b2f8de160d2b0c
Enkrl <Enkrllastlegion@gmail.com>
2021-10-26 20:24:02 +0300

    setup.py update gitignore

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name="TransUNet",
+    version='1.0',
+    description='Python Distribution Utilities',
+    install_requires=[
+        "torch==1.4.0",
+        "torchvision==0.5.0",
+        "numpy~=1.19.5",
+        "tqdm~=4.62.3",
+        "tensorboard",
+        "tensorboardX~=2.4",
+        "ml-collections",
+        "medpy~=0.4.0",
+        "SimpleITK~=2.1.1",
+        "scipy~=1.5.4",
+        "h5py~=3.1.0",
+        "synapseclient~=2.4.0",
+        "nibabel~=3.2.1"],
+    setup_requires=[
+        "torch==1.4.0",
+        "torchvision==0.5.0",
+        "numpy~=1.19.5",
+        "tqdm~=4.62.3",
+        "tensorboard",
+        "tensorboardX~=2.4",
+        "ml-collections",
+        "medpy~=0.4.0",
+        "SimpleITK~=2.1.1",
+        "scipy~=1.5.4",
+        "h5py~=3.1.0",
+        "synapseclient~=2.4.0",
+        "nibabel~=3.2.1"]
+)
