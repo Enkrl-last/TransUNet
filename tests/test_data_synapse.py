0a9fdb2b2d112101eaf417c1fcbb0df21e037efd
f10w <48291520+ffflowww@users.noreply.github.com>
2021-10-27 04:10:12 +0300

    Update test_data_synapse.py

--- tests/test_data_synapse.py
+++ tests/test_data_synapse.py
@@ -1,23 +1,27 @@
+""" Test Data Synapse """
 import unittest
-import datasets.dataset_synapse as ds
 import numpy as np
+import datasets.dataset_synapse as ds
 
 
 class TestGetData(unittest.TestCase):
+    """ Test Class for Data Synapse """
     #  Each test method starts with the keyword test_
     def test_rrf(self):
+        """ Test random_rot_flip """
         test_ar1 = np.ndarray([1, 2, 3])
         test_ar2 = np.ndarray([8, 7, 6])
-        a, b = ds.random_rot_flip(test_ar1, test_ar2)
-        self.assertIsInstance(a, np.ndarray)
-        self.assertIsInstance(b, np.ndarray)
+        a_a, b_b = ds.random_rot_flip(test_ar1, test_ar2)
+        self.assertIsInstance(a_a, np.ndarray)
+        self.assertIsInstance(b_b, np.ndarray)
 
     def test_rr(self):
+        """ Test random_rotate """
         test_ar1 = np.ndarray([5, 3, 4])
         test_ar2 = np.ndarray([3, 3, 2])
-        a, b = ds.random_rotate(test_ar1, test_ar2)
-        self.assertIsInstance(a, np.ndarray)
-        self.assertIsInstance(b, np.ndarray)
+        a_a, b_b = ds.random_rotate(test_ar1, test_ar2)
+        self.assertIsInstance(a_a, np.ndarray)
+        self.assertIsInstance(b_b, np.ndarray)
 
 
 if __name__ == "__main__":
