a6ef49cb1d6e701163548dfd440f0f66cddd9a93
f10w <48291520+ffflowww@users.noreply.github.com>
2021-10-27 04:10:35 +0300

    Update test_vsc.py

--- tests/test_vsc.py
+++ tests/test_vsc.py
@@ -1,21 +1,59 @@
+""" Test Vit Seg Configs """
 import unittest
-import networks.vit_seg_configs as vsc
 import ml_collections
+import networks.vit_seg_configs as vsc
 
 
-class TestGetData(unittest.TestCase):
+class TestFuncs(unittest.TestCase):
+    """ Test Class for Vit Seg Configs """
     #  Each test method starts with the keyword test_
     def test_b16(self):
-        a = ml_collections.ConfigDict()
-        b = vsc.get_b16_config()
-        self.assertIsInstance(a, type(b))
+        """ Test get_b16_config """
+        a_a = vsc.get_b16_config()
+        b_b = ml_collections.ConfigDict()
+        self.assertIsInstance(a_a, type(b_b))
 
-    # def test_rr(self):
-    #     test_ar1 = np.ndarray([5, 3, 4])
-    #     test_ar2 = np.ndarray([3, 3, 2])
-    #     a, b = ds.random_rotate(test_ar1, test_ar2)
-    #     self.assertIsInstance(a, np.ndarray)
-    #     self.assertIsInstance(b, np.ndarray)
+    def test_testing(self):
+        """ Test get_b16_config """
+        a_a = vsc.get_testing()
+        b_b = ml_collections.ConfigDict()
+        self.assertIsInstance(a_a, type(b_b))
+
+    def test_get_r50_b16_config(self):
+        """ Test get_b16_config """
+        a_a = vsc.get_r50_b16_config()
+        b_b = ml_collections.ConfigDict()
+        self.assertIsInstance(a_a, type(b_b))
+
+    def test_get_b32_config(self):
+        """ Test get_b16_config """
+        a_a = vsc.get_b32_config()
+        b_b = vsc.get_b16_config()
+        self.assertIsInstance(a_a, type(b_b))
+
+    def test_get_l16_config(self):
+        """ Test get_b16_config """
+        a_a = vsc.get_l16_config()
+        b_b = ml_collections.ConfigDict()
+        self.assertIsInstance(a_a, type(b_b))
+
+    def test_get_r50_l16_config(self):
+        """ Test get_b16_config """
+        a_a = vsc.get_r50_l16_config()
+        b_b = vsc.get_l16_config()
+        self.assertIsInstance(a_a, type(b_b))
+
+    def test_get_l32_config(self):
+        """ Test get_b16_config """
+        a_a = vsc.get_l32_config()
+        b_b = vsc.get_l16_config()
+        self.assertIsInstance(a_a, type(b_b))
+
+    def test_get_h14_config(self):
+        """ Test get_b16_config """
+        a_a = vsc.get_h14_config()
+        b_b = ml_collections.ConfigDict()
+        self.assertIsInstance(a_a, type(b_b))
 
 
 if __name__ == "__main__":
